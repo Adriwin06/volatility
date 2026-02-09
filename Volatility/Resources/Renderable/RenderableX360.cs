1084af10ad201002f05cda1af716557b3c4761ae
Nathan V. <jebcraftserver@gmail.com>
2026-02-09 13:56:15 -0500

    DrawIndexedParameters for xbox 360

--- Volatility/Resources/Renderable/RenderableX360.cs
+++ Volatility/Resources/Renderable/RenderableX360.cs
@@ -4,6 +4,17 @@ public class RenderableX360 : RenderableBase
 {
     public override Endian GetResourceEndian() => Endian.BE;
     public override Platform GetResourcePlatform() => Platform.X360;
+    
+    public override DrawIndexedParameters ParseDrawIndexedParameters(ResourceBinaryReader reader)
+    {
+        return new DrawIndexedParameters
+        {
+            GeometryPrimitiveType = GeometryPrimitiveTypeConverter.ToKind((D3DPRIMITIVETYPE)reader.ReadUInt32()),
+            BaseVertexIndex = reader.ReadInt32(),
+            StartIndex = reader.ReadUInt32(),
+            IndexCount = reader.ReadUInt32(),
+        };
+    }
 
     public RenderableX360(string path, Endian endianness = Endian.Agnostic) : base(path, endianness) { }
 }
